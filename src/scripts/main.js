0305766ee81c354bc406a87c99980020aee8c472
Sertavr <pampuh.net@gmail.com>
2025-11-02 21:40:33 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.left = '50%';
-spider.style.top = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+const centerX = wall.clientWidth / 2;
+const centerY = wall.clientHeight / 2;
+const spiderX = spider.clientWidth / 2;
+const spiderY = spider.clientHeight / 2;
+
+spider.style.left = `${centerX - spiderX}px`;
+spider.style.top = `${centerY - spiderY}px`;
