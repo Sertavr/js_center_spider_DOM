033dc6d3f87b6824788a0cb618adf0983b058e45
Sertavr <pampuh.net@gmail.com>
2025-11-02 21:41:38 +0200

    make build

---
+++ index.658a1ab6.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,l=e.clientHeight/2,i=t.clientWidth/2,n=t.clientHeight/2;t.style.left="".concat(c-i,"px"),t.style.top="".concat(l-n,"px");
+//# sourceMappingURL=index.658a1ab6.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const centerX = wall.clientWidth / 2;
+const centerY = wall.clientHeight / 2;
+const spiderX = spider.clientWidth / 2;
+const spiderY = spider.clientHeight / 2;
+spider.style.left = `${centerX - spiderX}px`;
+spider.style.top = `${centerY - spiderY}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
